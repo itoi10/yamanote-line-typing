06621f53d60e9425847a2e4a94219bb6c57e604d
itict10 <itict10@gmail.com>
2022-07-03 19:02:18 +0900

    緑色のサークル回転

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,6 @@ import NumberingIcon from "@/components/NumberingIcon";
 import Clock from "@/components/Clock";
 
 const Home: NextPage = () => {
-  const [inputValue, setInputValue] = useState("");
   const [currentNum, setCurrentNum] = useState<number>(0);
   const [dispName, setDispName] = useState<string[]>([]);
 
@@ -55,22 +54,22 @@ const Home: NextPage = () => {
                 {/* 時刻 */}
                 <Clock />
                 {/* 駅名 */}
-                <p className="flex w-full justify-between text-justify text-8xl">
+                <p className="flex w-full justify-between pl-5 text-justify text-8xl">
                   {dispName.map((chara, i) => (
                     <p key={i}>{chara}</p>
                   ))}
                 </p>
               </div>
             </div>
-            <div className="absolute z-10 w-full">
+            <div className="absolute z-10 flex w-full">
               {/* 乗り換え */}
               {stations[currentNum].transfers && (
-                <div>
+                <div className="w-full">
                   <b className="text-lg">{stations[currentNum].name}駅</b>
                   <p>乗換えのご案内</p>
-                  <div className="flex w-2/5 flex-row flex-wrap">
+                  <div className="flex w-3/5 flex-row flex-wrap">
                     {stations[currentNum].transfers?.map((transfer, i) => (
-                      <span key={i} className="bold w-1/2 p-1 px-2 text-lg font-bold">
+                      <span key={i} className="bold w-1/2 p-1 px-2  font-bold">
                         {transfer}
                       </span>
                     ))}
@@ -79,15 +78,19 @@ const Home: NextPage = () => {
               )}
             </div>
 
+            {/* 緑の曲線 */}
             <div
               className="border-large  absolute
-              right-1/3 top-0 inline-block
             rounded-full
-            border-green-400"
+            border-t-gray-300
+            border-l-gray-300
+            border-b-green-400
+            border-r-green-400
+            "
             ></div>
           </div>
 
-          <div className="absolute h-full w-full  border bg-gray-100">
+          <div className="absolute h-screen w-full  border bg-gray-100">
             <button onClick={click}>click</button>
           </div>
         </div>
